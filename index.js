80e7d1bc97223887fc8b1c819e9a86118e6e6926
moamin95 <mohammedamin1995@gmail.com>
2020-07-18 09:06:27 -0400

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
 
-require('dotenv').config();
+
 const app = express();
 
 //PASSPORT CONFIG
